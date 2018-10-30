4943f779db873a717eff9fe3d90020b0dd83d423
shimmy8 <gromov.91@gmail.com>
2018-10-30 11:51:34 +0300

    fix deprecated reeverse import

--- grappelli_menu/templatetags/admin_apps.py
+++ grappelli_menu/templatetags/admin_apps.py
@@ -1,7 +1,6 @@
 from django import template
 from django.utils.text import capfirst
-from django.urls import NoReverseMatch
-from django.core.urlresolvers import reverse
+from django.urls import NoReverseMatch, reverse
 from django.utils import six
 from django.contrib import admin
 
